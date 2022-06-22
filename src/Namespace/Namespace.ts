6c5014ccfc17f1dee4758d0207b83fdf45e104d4
PerceptControl <sa1rac.work@gmail.com>
2022-06-22 21:24:17 +0400

    Added solo-destination option to .control and logs

--- src/Namespace/Namespace.ts
+++ src/Namespace/Namespace.ts
@@ -1,28 +1,43 @@
 import type { eventName } from '../types'
 import type { destination, IDestination } from '../types/DestinationController'
-import type { finalData } from '../types/DataManager'
+import type { finalData } from '../types/DataManager.js'
 
 import { EventHandler, EventManager } from '../EventManager/EventManager.js'
 import DestinationConrtoller from '../DestinationConrtoller/DestinationConrtoller.js'
+import logger from '../Logger/Logger.js'
+
 export declare interface INamespace {
-  control(...destinations: Array<destination>): IDestination
+  control(destinations: destination | Array<destination>): IDestination
   emit(event: eventName, ...data: finalData[]): void
   on(event: eventName, callback: EventHandler): void
 }
 
 export class Namespace implements INamespace {
   constructor(private _name: string, private _events: EventManager) {}
-  control(...destinations: string[]): IDestination {
-    if (destinations[0] != 'broadcast')
-      throw Error(`${this._name}.control('broadcast') is reserved. Try to use ${this._name}.emit(...) instead`)
-    return DestinationConrtoller(this._name, ...destinations)
+  control(destinations: string | string[]): IDestination {
+    if (logger.flags.debug) logger.debug(`${this._name}: controlling ${destinations}`)
+    if (destinations instanceof Array) {
+      if (destinations[0] == 'broadcast')
+        logger.fatal(
+          `{ ${this._name}.control('broadcast') } is reserved. Try to use { ${this._name}.emit(...) } instead`,
+        )
+      return DestinationConrtoller(this._name, ...destinations)
+    } else {
+      if (destinations == 'broadcast')
+        logger.fatal(
+          `{ ${this._name}.control('broadcast') } is reserved. Try to use { ${this._name}.emit(...) } instead`,
+        )
+      return DestinationConrtoller(this._name, destinations)
+    }
   }
 
   emit(event: string, ...data: finalData[]): void {
-    this.control('broadcast').emit(event, data)
+    if (logger.flags.debug) logger.debug(`${this._name}: emitting '${event}'`)
+    this.control('reserved/broadcast').emit(event, data)
   }
 
   on(event: string, callback: EventHandler): void {
+    if (logger.flags.debug) logger.debug(`${this._name}: create handler "${event}"`)
     this._events.spawn(event, callback)
   }
 }

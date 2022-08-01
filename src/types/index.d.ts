99d03a63f46fbc9bb2842566b18fdbd03ed34dbe
PerceptControl <sa1rac.work@gmail.com>
2022-08-01 17:53:50 +0300

    added default type declarator

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,8 @@
+import type { AppOptions, TemplatedApp, WebSocket, us_listen_socket } from 'uWebSockets.js'
+import type { AliasPool } from '../AliasPool/AliasPool'
+import type Logger from '../Logger/Logger'
+import type { Namespace } from '../Namespace/Namespace'
+
 export declare type Handler<T> = (this: { id: escortID; name: eventName }, innerData: T) => void | Promise<void>
 
 export declare type escortID = string
@@ -17,4 +22,15 @@ export declare interface IManager<Escort> {
   get(id: escortID): Escort | undefined
   drop(id: escortID): boolean
   drop(escort: Escort): boolean
+}
+
+declare module 'gamesocket.io' {
+  export function io(options?: AppOptions): {
+    app: TemplatedApp
+    sockets: Map<socketID, WebSocket>
+    of: (name: string) => Namespace
+    listen: (port: number, callback: (ls: us_listen_socket) => void) => void
+    aliases: AliasPool
+    logger: Logger
+  }
 }

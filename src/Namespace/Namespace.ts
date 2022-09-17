af0cd872e48a6158bb83ee49cbaccdd7d42edec0
PerceptControl <sa1rac.work@gmail.com>
2022-09-18 01:57:21 +0300

    added: namespace inner socket&alias pools

--- src/Namespace/Namespace.ts
+++ src/Namespace/Namespace.ts
@@ -1,18 +1,24 @@
-import type { eventName } from '../io.js'
+import { AliasPool, eventName, IAliasPool } from '../io.js'
 import type { destination, IDestination } from '../DestinationConrtoller/DestinationConrtoller.js'
 import type { finalData } from '../DataManager/DataManager.js'
 
 import { EventHandler, EventManager } from '../EventManager/EventManager.js'
 import DestinationConrtoller from '../DestinationConrtoller/DestinationConrtoller.js'
 import logger from '../Logger/Logger.js'
+import { List } from '../SocketList/List.js'
 
 export declare interface INamespace {
   control(destinations: destination | Array<destination>): IDestination
   emit(event: eventName, ...data: finalData[]): void
   on(event: eventName, callback: EventHandler): void
+
+  get Aliases(): IAliasPool
+  get Sockets(): List<string>
 }
 
 export class Namespace implements INamespace {
+  private _aliases = new AliasPool()
+  private _sockets: List<string> = new List('undefiend')
   constructor(private _name: string, private _events: EventManager) {}
   get name() {
     return this._name
@@ -42,5 +48,13 @@ export class Namespace implements INamespace {
   on(event: string, callback: EventHandler): void {
     if (logger.flags.debug) logger.debug(`${this._name}: create handler "${event}"`)
     this._events.spawn(event, callback)
+  }
+
+  get Aliases(): IAliasPool {
+    return this._aliases
+  }
+
+  get Sockets() {
+    return this._sockets
   }
 }

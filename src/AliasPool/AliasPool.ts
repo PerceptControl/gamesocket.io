43a60e2f0de4bb52622c31d5ad114fa914fbd32d
PerceptControl <sa1rac.work@gmail.com>
2022-09-18 02:06:17 +0300

    fix: change modificators to public

--- src/AliasPool/AliasPool.ts
+++ src/AliasPool/AliasPool.ts
@@ -1,27 +1,27 @@
 import { socketID } from '../io.js'
 
 export declare abstract class IAliasPool {
-  static set(socketAlias: string, id: socketID): void
+  set(socketAlias: string, id: socketID): void
 
-  static swap(oldSocketAlias: string, newSocketAlias: string): boolean
+  swap(oldSocketAlias: string, newSocketAlias: string): boolean
 
-  static isSet(socketAlias: string): boolean
+  isSet(socketAlias: string): boolean
 
-  static get(socketAlias: string): Array<string> | undefined
+  get(socketAlias: string): Array<string> | undefined
 
-  static getId(socketAlias: string, id: socketID): void
+  getId(socketAlias: string, id: socketID): void
 
-  static remove(socketAlias: string): boolean
+  remove(socketAlias: string): boolean
 
-  static delete(socketAlias: string, id: socketID): boolean
+  delete(socketAlias: string, id: socketID): boolean
 }
 
 export class AliasPool implements IAliasPool {
-  private static aliases: Map<string, Array<string>> = new Map()
+  private _aliases: Map<string, Array<string>> = new Map()
   set(socketAlias: string, id: string): void {
-    let idArray = AliasPool.aliases.get(socketAlias)
+    let idArray = this._aliases.get(socketAlias)
 
-    if (!idArray) AliasPool.aliases.set(socketAlias, [id])
+    if (!idArray) this._aliases.set(socketAlias, [id])
     else idArray.push(id)
   }
 
@@ -30,32 +30,32 @@ export class AliasPool implements IAliasPool {
     if (!tmp) return false
 
     this.remove(oldSocketAlias)
-    AliasPool.aliases.set(newSocketAlias, tmp)
+    this._aliases.set(newSocketAlias, tmp)
     return true
   }
 
   isSet(socketAlias: string): boolean {
-    if (!AliasPool.aliases.get(socketAlias)) return false
+    if (!this._aliases.get(socketAlias)) return false
     return true
   }
 
   get(socketAlias: string): Array<string> | undefined {
-    return AliasPool.aliases.get(socketAlias)
+    return this._aliases.get(socketAlias)
   }
 
   getId(socketAlias: string, id: string): boolean {
-    let tmpArray = AliasPool.aliases.get(socketAlias)
+    let tmpArray = this._aliases.get(socketAlias)
     if (!tmpArray) return false
 
     return tmpArray.includes(id)
   }
 
   remove(socketAlias: string): boolean {
-    return AliasPool.aliases.delete(socketAlias)
+    return this._aliases.delete(socketAlias)
   }
 
   delete(socketAlias: string, id: string): boolean {
-    const buffer = AliasPool.aliases.get(socketAlias)
+    const buffer = this._aliases.get(socketAlias)
     if (!buffer) return false
 
     let index = buffer.indexOf(id)
